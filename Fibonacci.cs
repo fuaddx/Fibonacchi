88ba4a421d15bc444fd6d595d0b646b66583d2e2
fuaddx <147551899+fuaddx@users.noreply.github.com>
2023-10-12 23:51:05 +0400

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,45 @@
+﻿namespace Fibonacci_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Task 1:*/
+
+
+            /*Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 1, temp;
+            Console.Write("Fibonacci series: ");
+            Console.Write(a + ", ");
+
+            while (a < n)
+            {
+                temp = a;
+                a = b;
+                b = temp + b;
+                Console.Write(a);
+                if (a <= n)
+                {
+                    Console.Write(", ");
+                }
+            }*/
+
+            /*Task 2:*/
+
+
+            /*Console.WriteLine("Enter numbers: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 != n2)
+            {
+                n1 = n1 + n2;
+                n2 = n1 - n2;
+                n1 = n1 - n2;
+            }
+            Console.WriteLine("n1 => " + n1);
+            Console.WriteLine("n2 => " + n2);*/
+        }
+    }
+}
